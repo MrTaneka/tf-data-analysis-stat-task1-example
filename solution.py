e8febc6c8c5d65a25172284d7807ed19279affb1
taneka <112857258+MrTaneka@users.noreply.github.com>
2023-04-05 18:07:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 919511341
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    sum = 0.0
+    for i in range(0, len(x)):
+        if x[i] > 143:
+            sum = sum + math.log((x[i]-143))
+    sum = sum/len(x)
+    return sum
